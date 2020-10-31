d7caa2a37deb86bca6f5220f93218543de7c75f1
崔为俊 <834719201@qq.com>
2020-10-31 17:48:11 +0800

    live commit

---
+++ live/20201030/code/src/index.js
@@ -0,0 +1,6 @@
+// 这里我仅仅加载了模块中的 square 成员
+import { square } from './math.js'
+
+const ret = square(10)
+
+console.log(ret)
---
+++ live/20201030/code/src/math.js
@@ -0,0 +1,13 @@
+const a = 123
+
+export function square(x) {
+  console.log('square start')
+  return x * x
+
+  console.log('seuare end')
+}
+
+export function cube(x) {
+  console.log('cube')
+  return x * x * x
+}
